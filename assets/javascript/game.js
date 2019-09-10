80ac340ab08a4b1cd59c4c0ca874c77999acd5f0
Glen <ultrapancake@gmail.com>
2019-09-10 13:49:25 -0600

    On click Function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     //rock jpgs in an array with the source route steming from the HTML file, not the javaScript file.
     var rockImages =["assets/images/rock_1.jpg", "assets/images/rock_2.jpg", "assets/images/rock_3.jpg", "assets/images/rock_4.jpg"]
 
-    //Random Number generation for each different rock image
+    //Random Number generation for each different rock image, and create all classes and locations for rock images
     function rockNum(){
         for(var i=0; i<rockImages.length; i++){
             var rock = $("<img>");
@@ -15,5 +15,36 @@ $(document).ready(function(){
             rock.atter("data-letter", Math.floor(Math.random() *11) +1);
             $("#rocks").append(rock);
         }
+    }
+
+    //On click function
+    function onClick(){
+        $(".rock-button").on("click", function(){
+            // variables
+            rockClicked = true;
+            var rockValue = ($(this).attr("data-letter"));
+            rockValue = parseInt(rockValue);
+            counter += rockValue;
+            console.log(counter);
+            // push to HTML
+            $(".guess-user").html("Current points: " + counter);
+
+            // You Win If statement
+            if(counter === randNum){
+                wins++;
+                $(".wins").html("Wins: " + wins);
+                // Reset on Win
+                $("#rocks").empty();
+                rockNum();
+                game();
+            }else if (counter >= randNum){
+                losses++;
+                $(".losses").html("Losses: " + losses);
+                // Reset on Loss
+                $("#rocks").empty();
+                rockNum();
+                game();
+            }
+        });
     }
 })
