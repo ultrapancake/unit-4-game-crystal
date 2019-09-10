47379b0ff60d034cb2d79566d83d6af652371f11
Glen <ultrapancake@gmail.com>
2019-09-10 13:57:27 -0600

    Created game function to start and hold target Number generation; insereted rock generation function and start game function to bottom of file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,25 @@
 //Run javaScript after document loads
-$(document).ready(function(){
+$(document).ready(function () {
     //Global Variables
     var wins = 0;
     var losses = 0;
     //rock jpgs in an array with the source route steming from the HTML file, not the javaScript file.
-    var rockImages =["assets/images/rock_1.jpg", "assets/images/rock_2.jpg", "assets/images/rock_3.jpg", "assets/images/rock_4.jpg"]
+    var rockImages = ["assets/images/rock_1.jpg", "assets/images/rock_2.jpg", "assets/images/rock_3.jpg", "assets/images/rock_4.jpg"];
 
     //Random Number generation for each different rock image, and create all classes and locations for rock images
-    function rockNum(){
-        for(var i=0; i<rockImages.length; i++){
+    function rockNum() {
+        for (var i = 0; i < rockImages.length; i++) {
             var rock = $("<img>");
             rock.addClass("rock-button rock rock-img");
             rock.attr("src", rockImages[i]);
-            rock.atter("data-letter", Math.floor(Math.random() *11) +1);
+            rock.atter("data-letter", Math.floor(Math.random() * 11) + 1);
             $("#rocks").append(rock);
         }
-    }
+    };
 
     //On click function
-    function onClick(){
-        $(".rock-button").on("click", function(){
+    function onClick() {
+        $(".rock-button").on("click", function () {
             // variables
             rockClicked = true;
             var rockValue = ($(this).attr("data-letter"));
@@ -30,21 +30,36 @@ $(document).ready(function(){
             $(".guess-user").html("Current points: " + counter);
 
             // You Win If statement
-            if(counter === randNum){
+            if (counter === randNum) {
                 wins++;
                 $(".wins").html("Wins: " + wins);
                 // Reset on Win
                 $("#rocks").empty();
                 rockNum();
                 game();
-            }else if (counter >= randNum){
+            } else if (counter >= randNum) {
                 losses++;
                 $(".losses").html("Losses: " + losses);
                 // Reset on Loss
                 $("#rocks").empty();
                 rockNum();
                 game();
-            }
+            };
         });
+    };
+    function game() {
+        // variables
+        var counter = 0;
+        $(".guess-user").html("Current points: " + counter);
+        // Random Number Generation
+        var randNum = Math.floor(Math.random() * (111 - 3) + 3);
+        $(".guess-num").html("Target Number: " + randNum);
+        console.log(randNum);
+        // run onClick function
+        onClick();
     }
+    // Generate Rocks
+    rockNum();
+    // Start Game
+    game();
 })
